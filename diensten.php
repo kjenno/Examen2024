8e96231f48186d36cd2a4442ac287651a71e1545
Ahmed0046 <504655@vistacollege.nl>
2024-10-03 10:02:45 +0200

    diensten
    hierbij worden de producten van laten zien en kan worden gefilterd

--- diensten.php
+++ diensten.php
@@ -1,347 +1,32 @@
-<!-- <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
+<?php
+// Database verbinding
+$host = 'localhost'; // je database host
+$db = 'Hyperlight'; // je database naam
+$user = 'root'; // je database gebruikersnaam
+$pass = 'Islamic'; // je database wachtwoord
 
-    <link rel="stylesheet" href="./global.css" />
-    <link rel="stylesheet" href="./diensten.css" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap"
-    />
-  </head>
-  <body>
-    <div class="diensten">
-      <section class="main">
-        <header class="inloggen">
-          <div class="navbar-6">
-            <div class="content">
-              <div class="content1">
-                <img
-                  class="color-dark"
-                  loading="lazy"
-                  alt=""
-                  src="./public/color--dark.svg"
-                  id="colorDark"
-                />
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Verbinding mislukt: " . $e->getMessage();
+}
 
-                <nav class="column">
-                  <div class="link-text">Over Ons</div>
-                  <div class="link-text1" id="linkText1">Diensten</div>
-                  <div class="link-text2" id="linkText2">Projecten</div>
-                  <div class="nav-link-dropdown">
-                    <div class="link-text3">Planning</div>
-                  </div>
-                </nav>
-              </div>
-              <button
-                class="stylesecondary-smalltrue-a"
-                id="styleSecondarySmallTrueA"
-              >
-                <div class="button">inloggen</div>
-              </button>
-            </div>
-          </div>
-        </header>
-        <div class="header-54" data-scroll-to="header54">
-          <h1 class="short-heading-here">diensten</h1>
-          <div class="lorem-ipsum-dolor">
-            vraag uw offerte eenvoudig aan en bekijk onze producten
-          </div>
-        </div>
-        <div class="product-1">
-          <div class="title">
-            <div class="section-title">
-              <div class="tagline-wrapper"></div>
-              <div class="content2">
-                <h1 class="heading">Producten</h1>
-                <div class="text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-              </div>
-            </div>
-            <div class="actions"></div>
-          </div>
-          <div class="content3">
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image@2x.png"
-              />
+// Haal categorieën op uit de database
+$categorieQuery = "SELECT DISTINCT categorie FROM products";
+$categorieStmt = $pdo->prepare($categorieQuery);
+$categorieStmt->execute();
+$categorieen = $categorieStmt->fetchAll(PDO::FETCH_ASSOC);
 
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image@2x.png"
-              />
+// Haal producten op op basis van de gekozen categorie
+$gekozenCategorie = isset($_GET['categorie']) ? $_GET['categorie'] : 'Geluid'; // Standaard categorie is 'Geluid'
 
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image@2x.png"
-              />
-
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image@2x.png"
-              />
-
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image-4@2x.png"
-              />
-
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image-4@2x.png"
-              />
-
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image-4@2x.png"
-              />
-
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt=""
-                src="./public/placeholder-image-4@2x.png"
-              />
-
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
-                </div>
-                <div class="price">vraag uw offerte aan</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="container-wrapper">
-        <div class="container">
-          <div class="content12">
-            <div class="section-title1">
-              <div class="tagline-wrapper1">
-                <div class="tagline">Contact</div>
-              </div>
-              <div class="content13">
-                <h1 class="heading">Vraag een offerte aan!</h1>
-                <div class="text">
-                  Heb je vragen of hulp nodig? Neem contact met ons op via
-                  onderstaand formulier.
-                </div>
-              </div>
-            </div>
-            <div class="content14">
-              <div class="row">
-                <img
-                  class="icon-envelope"
-                  loading="lazy"
-                  alt=""
-                  src="./public/icon--envelope.svg"
-                />
-
-                <div class="link-text">support@hyperlightandsound.com</div>
-              </div>
-              <div class="row">
-                <img
-                  class="icon-envelope"
-                  loading="lazy"
-                  alt=""
-                  src="./public/icon--phone.svg"
-                />
-
-                <div class="link-text">+31 (0)123 456789</div>
-              </div>
-              <div class="row">
-                <img
-                  class="icon-envelope"
-                  loading="lazy"
-                  alt=""
-                  src="./public/icon--map.svg"
-                />
-
-                <div class="link-text">Straatnaam 1, 1234 AB Amsterdam</div>
-              </div>
-            </div>
-          </div>
-          <div class="form">
-            <div class="inputs">
-              <div class="input">
-                <div class="first-name">Voornaam</div>
-                <input class="typedefault-alternatefalse" type="text" />
-              </div>
-              <div class="input">
-                <div class="first-name">Achternaam</div>
-                <input class="typedefault-alternatefalse" type="text" />
-              </div>
-            </div>
-            <div class="inputs">
-              <div class="input">
-                <div class="first-name">E-mail</div>
-                <input class="typedefault-alternatefalse" type="text" />
-              </div>
-              <div class="input">
-                <div class="first-name">Telefoonnummer</div>
-                <input class="typedefault-alternatefalse" type="text" />
-              </div>
-            </div>
-            <div class="input4">
-              <div class="first-name">Bericht</div>
-              <div class="alternatefalse">
-                <div class="type-your-message">Typ hier je bericht...</div>
-                <img class="alternatefalse-child" loading="lazy" alt="" />
-
-                <img class="alternatefalse-item" alt="" />
-              </div>
-            </div>
-            <div class="selectedfalse-alternatefals">
-              <div class="checkbox"></div>
-              <div class="label">Ik accepteer de voorwaarden</div>
-            </div>
-            <button class="styleprimary-smallfalse-al">
-              <div class="button1">Verzend</div>
-            </button>
-          </div>
-        </div>
-      </section>
-      <section class="footer-7">
-        <div class="content15">
-          <div class="logo">
-            <img
-              class="color-dark1"
-              loading="lazy"
-              alt=""
-              src="./public/color--dark.svg"
-            />
-          </div>
-          <div class="links">
-            <div class="link-one">Over ons</div>
-            <div class="link-one">Diensten</div>
-            <div class="link-one">Projecten</div>
-            <div class="tagline">Contact</div>
-            <div class="link-one">Blog</div>
-          </div>
-        </div>
-      </section>
-    </div>
-
-    <script>
-      var colorDark = document.getElementById("colorDark");
-      if (colorDark) {
-        colorDark.addEventListener("click", function (e) {
-          window.location.href = "./index.html";
-        });
-      }
-      
-      var linkText1 = document.getElementById("linkText1");
-      if (linkText1) {
-        linkText1.addEventListener("click", function () {
-          var anchor = document.querySelector("[data-scroll-to='header54']");
-          if (anchor) {
-            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
-          }
-        });
-      }
-      
-      var linkText2 = document.getElementById("linkText2");
-      if (linkText2) {
-        linkText2.addEventListener("click", function (e) {
-          window.location.href = "./projecten.html";
-        });
-      }
-      
-      var styleSecondarySmallTrueA = document.getElementById(
-        "styleSecondarySmallTrueA"
-      );
-      if (styleSecondarySmallTrueA) {
-        styleSecondarySmallTrueA.addEventListener("click", function (e) {
-          window.location.href = "./login.html";
-        });
-      }
-      </script>
-  </body>
-</html> -->
-
-
+$sql = "SELECT naam, foto FROM products WHERE categorie = :categorie";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':categorie', $gekozenCategorie, PDO::PARAM_STR);
+$stmt->execute();
+$producten = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
 <!DOCTYPE html>
 <html>
@@ -362,7 +47,7 @@
           <div class="navbar-6">
             <div class="content">
               <div class="content1">
-                <a href="./index.html">
+                <a href="./index.php">
                   <img
                     class="color-dark"
                     loading="lazy"
@@ -372,13 +57,13 @@
                 </a>
 
                 <nav class="column">
-                  <a href="./over-ons.html" class="link-text">Over Ons</a>
+                  <a href="./over-ons.php" class="link-text">Over Ons</a>
                   <a href="#header54" class="link-text1">Diensten</a>
-                  <a href="./projecten.html" class="link-text2">Projecten</a>
-                  <a href="./planning.html" class="link-text3">Planning</a>
+                  <a href="./projecten.php" class="link-text2">Projecten</a>
+                  <a href="./planning.php" class="link-text3">Planning</a>
                 </nav>
               </div>
-              <a href="./login.html" class="stylesecondary-smalltrue-a">
+              <a href="./login.php" class="stylesecondary-smalltrue-a">
                 <div class="button">Inloggen</div>
               </a>
             </div>
@@ -392,38 +77,52 @@
           </div>
         </div>
 
+        <!-- Categorie Selectie -->
+        <form method="GET" class="category-selector">
+          <label for="categorie">Kies een categorie:</label>
+          <select name="categorie" id="categorie" onchange="this.form.submit()">
+            <?php foreach ($categorieen as $categorie): ?>
+              <option value="<?php echo htmlspecialchars($categorie['categorie']); ?>" <?php echo ($categorie['categorie'] == $gekozenCategorie) ? 'selected' : ''; ?>>
+                <?php echo htmlspecialchars($categorie['categorie']); ?>
+              </option>
+            <?php endforeach; ?>
+          </select>
+        </form>
+
         <div class="product-1">
           <div class="title">
             <div class="section-title">
               <div class="tagline-wrapper"></div>
               <div class="content2">
-                <h1 class="heading">Producten</h1>
-                <div class="text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
+                <h1 class="heading">Producten in categorie: <?php echo htmlspecialchars($gekozenCategorie); ?></h1>
+                <div class="text">Bekijk onze producten in de geselecteerde categorie.</div>
               </div>
             </div>
             <div class="actions"></div>
           </div>
 
           <div class="content3">
-            <!-- Herhaal dit blok voor elk product -->
-            <div class="product">
-              <img
-                class="placeholder-image-icon"
-                loading="lazy"
-                alt="Placeholder Afbeelding"
-                src="./public/placeholder-image@2x.png"
-              />
-              <div class="content4">
-                <div class="header">
-                  <div class="heading1">Product name</div>
-                  <div class="product-description">Variant</div>
+            <?php if (count($producten) > 0): ?>
+              <?php foreach ($producten as $product): ?>
+                <div class="product">
+                  <img
+                    class="placeholder-image-icon"
+                    loading="lazy"
+                    alt="<?php echo htmlspecialchars($product['naam']); ?>"
+                    src="<?php echo htmlspecialchars($product['foto']); ?>"
+                  />
+                  <div class="content4">
+                    <div class="header">
+                      <div class="heading1"><?php echo htmlspecialchars($product['naam']); ?></div>
+                      <div class="product-description">Variant</div>
+                    </div>
+                    <div class="price">Vraag uw offerte aan</div>
+                  </div>
                 </div>
-                <div class="price">Vraag uw offerte aan</div>
-              </div>
-            </div>
-            <!-- Einde herhaling -->
+              <?php endforeach; ?>
+            <?php else: ?>
+              <p>Geen producten beschikbaar in deze categorie.</p>
+            <?php endif; ?>
           </div>
         </div>
       </section>
@@ -501,18 +200,18 @@
 
       <section class="footer-7">
         <div class="content15">
-          <a href="./index.html">
+          <a href="./index.php">
             <div class="logo">
               <img class="color-dark1" loading="lazy" alt="Logo" src="./public/color--dark.svg" />
             </div>
           </a>
 
           <div class="links">
-            <a href="./over-ons.html" class="link-one">Over ons</a>
+            <a href="./over-ons.php" class="link-one">Over ons</a>
             <a href="#header54" class="link-one">Diensten</a>
-            <a href="./projecten.html" class="link-one">Projecten</a>
-            <a href="./contact.html" class="tagline">Contact</a>
-            <a href="./blog.html" class="link-one">Blog</a>
+            <a href="./projecten.php" class="link-one">Projecten</a>
+            <a href="./contact.php" class="tagline">Contact</a>
+            <a href="./blog.php" class="link-one">Blog</a>
           </div>
         </div>
       </section>
