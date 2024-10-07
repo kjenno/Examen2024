42cc9a17799bd32527b2c351f2aaea7d6b82464a
kjenno <507598@vistacollege.nl>
2024-10-07 09:59:46 +0200

    factuurpagina
    Pagina voor de klant en de admin om facturen toe te voegen en in te zien.

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php
+// Load Composer's autoloader
+require 'vendor/autoload.php'; 
+
+// Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+
+function MailSender($ReceiverMail,$fileDestination,$MSubject,$MText)
+{
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'hyperlightandsoundtest123@gmail.com';  // SMTP username (your Gmail)
+    $mail->Password   = 'txnb gxkt bkje zagu';                    // SMTP password (Use Gmail app-specific password)
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to (TLS)
+
+    //Recipients
+    $mail->setFrom('hyperlightandsoundtest123@gmail.com', 'HyperLightandSound');  // Set your email
+    $mail->addAddress($ReceiverMail);                           // Add a recipient
+
+    if($MText != "")
+    {
+        $mail->Body    = $MText;
+    }
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $MSubject;
+
+    $mail->addAttachment($fileDestination);
+
+    $mail->send();
+    echo 'Message has been sent';
+}
+
+?>
